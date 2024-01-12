db7256afd744b3a71459de80146de5794f64feac
dLucs <lucas_61@hotmail.es>
2024-01-12 12:16:47 +0100

    Initial Commit

---
+++ Sleeper/SleeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleeperApp.swift
+//  Sleeper
+//
+//  Created by Lucas on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SleeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

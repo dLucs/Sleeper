6cca8eb4cc95f814624fc50958885d23aad8ecb7
dLucs <lucas_61@hotmail.es>
2024-01-12 12:33:00 +0100

    added basic UI

--- Sleeper/ContentView.swift
+++ Sleeper/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var  wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper ("\(sleepAmount .formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                Text("Daile coffe intake")
+                    .font(.headline)
+                Stepper ("\(coffeeAmount .formatted()) cup(s)", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("Sleeper")
+            .toolbar{
+                Button("Calculate", action: calcBedtime)
+            }
         }
-        .padding()
+        
+    }
+    func calcBedtime(){
+        
     }
 }
 
